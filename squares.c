a91ffdcabd7163a9a24841c65509f07a76ad3f1c
LorenzoM1997 <lorenz.m97@gmail.com>
2019-09-19 16:08:37 -0700

    added analysis for case with two triples

--- squares.c
+++ squares.c
@@ -13,14 +13,15 @@ int main(){
 	double start, increment, square;
 	double saved[128];
 	double temp;
-  short i, j, k, c, len;
+  short i, j, k, c, len, z;
 	short couples, triples, count;
 	double distance;
+	bool detected = false;
 	int r;
 
 	double trios[9];
 
-	for (increment = 40000 ;increment < 10e7; increment++){
+	for (increment = 1 ;increment < 10e7; increment++){
 
 		// numbers ending in 2,3,7,8 can't lead to a group of 3 squares
 		r = (int)increment % 10;
@@ -50,9 +51,9 @@ int main(){
 					temp = square;
 				}
 				else if(count >= 3){
-					trios[triples+2] = square*square;
-					trios[triples+1] = trios[triples+2] - increment;
-					trios[triples] = trios[triples+1] - increment;
+					trios[triples*3+2] = square*square;
+					trios[triples*3+1] = trios[triples*3+2] - increment;
+					trios[triples*3] = trios[triples*3+1] - increment;
 					i --;
 					temp = square;
 					couples --; // remove one couple
@@ -66,43 +67,93 @@ int main(){
 				start = square * square;
 			}
 		}
-		if (couples + triples >= 3 && triples >= 1){
-			printf("increment: %.0lf, couples: %d, triples: %d\n", increment, couples, triples);
+		if ((triples==1 && couples>=2)||(triples>=2)){
 
-			// find triples and take them out from pool
-			for (c = 0; c < i; c++){
-				printf("%.0lf ", saved[c]);
-			}
+			if (triples == 1){
 
-			// compute distance from triples end point
-			len = 0;
-			double distances[triples*i*2];
-			for (c = 0; c < i; c ++){
-				for (j = 0; j < triples*3; j+=3){
-					distances[len] = saved[c] * saved[c] - trios[j+2];
-					distances[len + 1] = distances[len] - increment;
-					if (distances[len] < 0) distances[len] *= (-1);
-					if (distances[len+1]< 0) distances[len+1] *= (-1);
-					len += 2;
+				// compute distance from triples end point
+				len = 0;
+				double distances[triples*i*2];
+				for (c = 0; c < i; c ++){
+					for (j = 0; j < triples*3; j+=3){
+						distances[len] = saved[c] * saved[c] - trios[j+2];
+						distances[len + 1] = distances[len] - increment;
+						if (distances[len] < 0) distances[len] *= (-1);
+						if (distances[len+1]< 0) distances[len+1] *= (-1);
+						len += 2;
 
+					}
 				}
-			}
 
-			// check if any two distances is duplicate
-			for (k = 1; k < i; k ++){
-				for (j = 0; j < k; j ++){
-					distance = saved[k]*saved[k] - saved[j]*saved[j];
-					for (c = 0; c < len; c ++){
-						if (distance == distances[c]){
-							printf("%.0lf ***\n", distances[c]);
-						}
-						else if(distance - increment == distances[c]){
-							printf("%.0lf ***\n", distance - increment);
+				// check if any two distances is duplicate
+				for (k = 1; k < i; k ++){
+					for (j = 0; j < k; j ++){
+						distance = saved[k]*saved[k] - saved[j]*saved[j];
+						for (c = 0; c < len; c ++){
+							if (distance == distances[c]) detected = true;
+							else if(distance - increment == distances[c]) detected = true;
+							else if(distance + increment == distances[c]) detected = true;
+							if (detected) {
+								printf("Increment: %.0lf, couples: %d, triples: %d\n", increment, couples, triples);
+								// print detected elements
+								printf(" Couples end point: %.0lf %.0lf", saved[k], saved[j]);
+								printf("\n Triples:");
+								for (z = 0; z < triples; z ++){
+									printf("%.0lf %.0lf %.0lf", trios[z*3], trios[z*3+1], trios[z*3+2]);
+								}
+								printf("\n Distance: %.0lf\n", distances[c]);
+								detected = false;
+							}
 						}
 					}
 				}
 			}
-			printf("\n");
+			else{  // if there is more than one triple
+
+				for (j = 1; j < triples;j++){
+					for(c = 0; c< j; c ++){
+						// measure the distance
+						distance = trios[j*3] - trios[c*3];
+
+						// check below if there is a square number at the same distance
+						square = 1;
+						if (trios[c*3] - distance > 0){
+							for (k = 0; k < 3; k++){
+								while(trios[c*3 + k] - distance > square * square) square ++;
+								if (trios[c*3 + k] - distance == square * square){
+									printf("increment: %.0lf, couples: %d, triples: %d\n", increment, couples, triples);
+									// print detected elements
+									for (c = 0; c < i; c++){
+										printf("%.0lf ", saved[c]);
+									}
+									printf("\n");
+									printf("NEW (below)\n");
+									printf("%.0lf %.0lf", trios[c*3], trios[j*3]);
+								}
+							}
+						}
+
+						// check above if there is a square number at the same distance
+						for (k = 0; k < 3; k++){
+							while(trios[j*3 + k] + distance > square*square) square++;
+							if (trios[j*3 + k] + distance == square * square){
+								printf("increment: %.0lf, couples: %d, triples: %d\n", increment, couples, triples);
+								// print detected elements
+								for (c = 0; c < i; c++){
+									printf("%.0lf ", saved[c]);
+								}
+								printf("\n");
+								printf("NEW (above)\n");
+								printf("%.0lf %.0lf", trios[c*3], trios[j*3]);
+							}
+						}
+
+					}
+				}
+
+
+
+			}
 		}
 	}
 	return 0;
